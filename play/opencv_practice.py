ea5e0a9038b6ba6f19841441bf57641865e00e12
stretchyDawg <cmmorgado53@gmail.com>
2024-09-18 14:40:12 -0400

    openCV practice

---
+++ play/opencv_practice.py
@@ -0,0 +1,39 @@
+import cv2
+import random
+import os
+
+def main(): 
+    # Create the 'output' folder if it doesn't exist
+    output_folder = 'play/output'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    # Flags:
+    # -1, cv2.IMREAD_COLOR: Default, Loads image in color, Transparency will be neglected
+    # 0, cv2.IMREAD_GRAYSCALE: Loads image in grayscale
+    # 1, cv2.IMREAD_UNCHANGED: Loads image in color, Transparency will be saved
+    img = cv2.imread('assets/STOP_SIGN.png', 1)
+    img = cv2.resize(img, (400, 400))
+    cv2.imwrite(os.path.join(output_folder, 'opencv_play_img.png'), img)
+    
+    print(img, end="\n\n") # read values as 'blue, green, red'
+    print(img[20], end="\n\n")         # one row
+    print(img[20][10:40], end="\n\n")  # collection of pixels in row
+    print(img[20][20], end="\n\n")     # one pixel
+    print("Shape:", img.shape) # (Height, width, channels)
+    
+    # changing
+    for i in range(150):
+        for j in range(img.shape[1]):
+            img[i][j] = [random.randint(200, 255), random.randint(0, 255), random.randint(0, 0)]
+    
+    # copying and pasting
+    tag = img[150:250, 300:400]
+    img[1:101, 300:400] = tag
+    
+    cv2.imshow('Image',  img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
