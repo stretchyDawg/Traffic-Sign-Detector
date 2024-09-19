8f156520c354bf836944b5b3682523a600cf5a17
stretchyDawg <cmmorgado53@gmail.com>
2024-09-19 08:11:35 -0400

    COlor masking

--- play/video_practice.py
+++ play/video_practice.py
@@ -9,17 +9,16 @@ def main():
         width = int(capture.get(3))
         height = int(capture.get(4)) # 4 is the 3rd property of a capture, look up documentation for more properties
         
-        #               (starting pos, ending pos, color, line thickness)
-        image = cv2.line(frame, (0,0), (width, height), (255, 0, 0), 10)
-        image = cv2.line(frame, (width,0), (0, height), (0, 0, 255), 10)
-        #                    (top left corner, bottom right corner, color, thickness)
-        image = cv2.rectangle(image, (100, 100), (200, 200), (128,128,128), 5)
-        #                 (center, radius, color, thickness)
-        image = cv2.circle(image, (300, 300), 100, (0,255,0), -1)
+        # Hue Saturation Value
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # BGR to HSV
+        lower_blue = np.array([90, 50, 50])
+        upper_blue = np.array([130, 255, 255])
         
-        image = cv2.putText(image, "I have to fart.", (10, height - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5, cv2.LINE_AA)
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        result = cv2.bitwise_and(frame, frame, mask=mask)
         
-        cv2.imshow('Video Frame', image)
+        cv2.imshow('Video Frame', result)
+        cv2.imshow('mask', mask)
         
         if(cv2.waitKey(1) == ord('q')):
             break
