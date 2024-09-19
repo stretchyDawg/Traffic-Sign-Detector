f5f26e047d7045e73923246ca9e82946ee0079e6
stretchyDawg <cmmorgado53@gmail.com>
2024-09-19 09:18:10 -0400

    Editing pixels

--- play/opencv_practice.py
+++ play/opencv_practice.py
@@ -13,7 +13,7 @@ def main():
     # 1, cv2.IMREAD_UNCHANGED: Loads image in color, Transparency will be saved
     img = cv2.imread('assets/STOP_SIGN.png', 1) # 0,0 is in the TOP LEFT
     img = cv2.resize(img, (400, 400))
-    cv2.imwrite(os.path.join(output_folder, 'opencv_play_img.png'), img)
+    cv2.imwrite(os.path.join(output_folder, 'opencv_play_img_original.png'), img)
     
     print(img, end="\n\n")             # read values as 'blue, green, red'
     print(img[20], end="\n\n")         # one row
@@ -21,7 +21,7 @@ def main():
     print(img[20][20], end="\n\n")     # one pixel
     print("Shape:", img.shape)         # (Height, width, channels)
     
-    # changing
+    # editing pixels
     for i in range(150):
         for j in range(img.shape[1]):
             img[i][j] = [random.randint(200, 255), random.randint(0, 255), random.randint(0, 0)]
@@ -29,6 +29,7 @@ def main():
     # copying and pasting
     tag = img[150:250, 300:400]
     img[1:101, 300:400] = tag
+    cv2.imwrite(os.path.join(output_folder, 'opencv_play_img_edited.png'), img)
     
     cv2.imshow('Image',  img)
     cv2.waitKey(0) # 0 is wait infinitely
